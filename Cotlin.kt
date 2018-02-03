9cb33ace9ff1a9ef5e7948fc9b29556eb76275ab
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2018-02-03 13:52:35 +0400

    Create Cotlin.kt

---
+++ Cotlin.kt
@@ -0,0 +1,11 @@
+class Cotlin {
+   private var name: String = "sololearn"
+   
+   fun output() {
+      print("Welcome to awesome "+name)
+   }
+}
+fun main(args: Array<String>) {
+   val demo = Cotlin()
+   demo.output();
+}
